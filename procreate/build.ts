ec883a880bc24d43a01c78e7d9602abf6b1780dd
winston <hey@winston.sh>
2022-12-29 20:29:09 +0100

    style: reformat after editorconfig change

--- procreate/build.ts
+++ procreate/build.ts
@@ -4,26 +4,26 @@ import { createSwatchesFile } from "npm:procreate-swatches";
 const capitalize = (str: string) => str[0].toUpperCase() + str.slice(1);
 
 type Color = {
-	hex: string;
-	rgb: string;
-	hsl: string;
+  hex: string;
+  rgb: string;
+  hsl: string;
 };
 type Palette = Record<string, Color>;
 
 const library: Palette[] = JSON.parse(
-	await Deno.readTextFile("../palette.json")
+  await Deno.readTextFile("../palette.json"),
 );
 
 for (const palette in library) {
-	const colours: Palette = library[palette];
+  const colours: Palette = library[palette];
 
-	const rgbValues = Object.values(colours).map((colour) => [
-		colour.rgb.match(/\d+/g),
-		"rgb",
-	]);
+  const rgbValues = Object.values(colours).map((colour) => [
+    colour.rgb.match(/\d+/g),
+    "rgb",
+  ]);
 
-	const name = `Catppuccin ${capitalize(palette)}`;
-	const data: ArrayBuffer = await createSwatchesFile(name, rgbValues);
+  const name = `Catppuccin ${capitalize(palette)}`;
+  const data: ArrayBuffer = await createSwatchesFile(name, rgbValues);
 
-	Deno.writeFileSync(`./${name}.swatches`, new Uint8Array(data));
+  Deno.writeFileSync(`./${name}.swatches`, new Uint8Array(data));
 }
