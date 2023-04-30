75d387569309612f954fcc17437c3018f9e46db8
winston <hey@winston.sh>
2023-04-30 22:35:31 +0200

    refactor: generate procreate via node

--- src/scripts/build_palettes.mjs
+++ src/scripts/build_palettes.mjs
@@ -1,19 +1,38 @@
-import fs from "fs/promises";
+import fs from "fs";
 import crypto from "crypto";
 import { variants } from "@catppuccin/palette";
 import url from "url";
 import path from "path";
+import { createSwatchesFile } from "procreate-swatches";
 
 const root = path.resolve(
   url.fileURLToPath(new URL(".", import.meta.url)),
   "..",
   "..",
-  "palettes"
+  "dist"
 );
 const dateString = new Date()
   .toISOString()
   .replace(/T/, " ")
   .replace(/\..+/, "");
+
+const generateGimp = (name, palette) => {
+  const n = Object.keys(palette).length;
+  const head = `GIMP Palette\n#Palette Name: ${name}\n#Colors: ${n}\n`;
+  const pad = (str) => str.toString().padStart(3, " ");
+  const body = Object.values(palette)
+    .map((value) => {
+      const v = [
+        pad(value.rgb.r),
+        pad(value.rgb.g),
+        pad(value.rgb.b),
+        value.hex,
+      ];
+      return v.join(" ");
+    })
+    .join("\n");
+  return head + body;
+};
 
 const generateSip = (name, palette) => {
   const data = {
@@ -47,33 +66,30 @@ const generateSip = (name, palette) => {
   return JSON.stringify(data, null, 2);
 };
 
-const generateGimp = (name, palette) => {
-  const n = Object.keys(palette).length;
-  const head = `GIMP Palette\n#Palette Name: ${name}\n#Colors: ${n}\n`;
-  const pad = (str) => str.toString().padStart(3, " ");
-  const palette = Object.values(palette)
-    .map((value) => {
-      const v = [
-        pad(value.rgb.r),
-        pad(value.rgb.g),
-        pad(value.rgb.b),
-        value.hex,
-      ];
-      return v.join(" ");
-    })
-    .join("\n");
-  return head + palette;
+const generateProcreate = async (name, palette) => {
+  const rgbValues = Object.values(palette).map((colour) => [
+    [colour.rgb.r, colour.rgb.g, colour.rgb.b],
+    "rgb",
+  ]);
+  return await createSwatchesFile(name, rgbValues);
 };
 
-Object.entries(variants).map(([name, palette]) => {
+Object.entries(variants).map(async ([name, palette]) => {
   name = `Catppuccin ${name.charAt(0).toUpperCase() + name.slice(1)}`;
 
-  const sip = generateSip(name, palette);
-  const gimp = generateGimp(name, palette);
-  const folders = ["sip", "gimp"];
-  folders.map((folder) => {
-    fs.mkdir(path.join(root, folder), { recursive: true });
-  });
-  fs.writeFile(path.resolve(root, `sip/${name}.palette`), sip);
-  fs.writeFile(path.resolve(root, `gimp/${name}.gpl`), gimp);
+  ["gimp", "sip", "procreate"].map((folder) =>
+    fs.mkdirSync(path.join(root, folder), { recursive: true })
+  );
+  fs.writeFileSync(
+    path.resolve(root, `sip/${name}.palette`),
+    generateSip(name, palette)
+  );
+  fs.writeFileSync(
+    path.resolve(root, `gimp/${name}.gpl`),
+    generateGimp(name, palette)
+  );
+  fs.writeFileSync(
+    path.resolve(root, `procreate/${name}.swatches`),
+    await generateProcreate(name, palette)
+  );
 });
