35bfbf6cede8b31421335e3376a1b7bbdaae6eab
winston <hey@winston.sh>
2023-07-10 04:18:04 +0200

    test: fix

---
+++ test.mjs
@@ -0,0 +1,40 @@
+import test from "ava";
+import { labels, variants } from "./dist/index.mjs";
+
+test("Labels", (t) => {
+  t.deepEqual(labels.base, {
+    latte: {
+      hex: "#eff1f5",
+      rgb: "rgb(239, 241, 245)",
+      raw: "239, 241, 245",
+      hsl: "hsl(220, 23%, 95%)",
+    },
+    frappe: {
+      hex: "#303446",
+      rgb: "rgb(48, 52, 70)",
+      raw: "48, 52, 70",
+      hsl: "hsl(229, 19%, 23%)",
+    },
+    macchiato: {
+      hex: "#24273a",
+      rgb: "rgb(36, 39, 58)",
+      raw: "36, 39, 58",
+      hsl: "hsl(232, 23%, 18%)",
+    },
+    mocha: {
+      hex: "#1e1e2e",
+      rgb: "rgb(30, 30, 46)",
+      raw: "30, 30, 46",
+      hsl: "hsl(240, 21%, 15%)",
+    },
+  });
+});
+
+test("Variants", (t) => {
+  t.deepEqual(variants.macchiato.rosewater, {
+    hex: "#f4dbd6",
+    rgb: "rgb(244, 219, 214)",
+    raw: "244, 219, 214",
+    hsl: "hsl(10, 58%, 90%)",
+  });
+});
