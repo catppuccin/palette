25ac4a925a6e78d370a7500fc3db60317d7c1261
uncenter <47499684+uncenter@users.noreply.github.com>
2024-07-20 21:09:55 -0400

    feat: add support for apple color list / .clr files

--- scripts/build_palettes.ts
+++ scripts/build_palettes.ts
@@ -4,6 +4,7 @@ import { join } from "std/path/mod.ts";
 import { flavors } from "@catppuccin/palette";
 import {
   generateAse,
+  generateClrJson,
   generateGimp,
   generatePng,
   generateProcreate,
@@ -14,14 +15,14 @@ const ROOT = new URL("../dist/palettes", import.meta.url).pathname;
 await emptyDir(ROOT);
 
 await Promise.all(
-  ["ase", "gimp", "procreate", "png", "sip"].map((folder) =>
+  ["ase", "gimp", "procreate", "png", "sip", "clr"].map((folder) =>
     ensureDir(join(ROOT, folder))
   ),
 );
 
 Promise.all(
-  Object.entries(flavors).flatMap(async ([name, { colors }]) => {
-    const fname = name.charAt(0).toUpperCase() + name.slice(1);
+  Object.entries(flavors).flatMap(async ([identifier, { name, colors }]) => {
+    const fname = identifier.charAt(0).toUpperCase() + identifier.slice(1);
 
     await Deno.writeFile(
       join(ROOT, `ase/${fname}.ase`),
@@ -43,5 +44,26 @@ Promise.all(
       join(ROOT, `sip/${fname}.palette`),
       generateSip(fname, colors),
     );
+
+    const clrJson = join(ROOT, `clr/${fname}.json`);
+    await Deno.writeTextFile(
+      clrJson,
+      generateClrJson(fname, colors),
+    );
+    const cmd = new Deno.Command("swift", {
+      args: [
+        join(import.meta.dirname!, "./builders/palettes/json-to-clr.swift"),
+        clrJson,
+        join(ROOT, `clr/Catppuccin ${name}.clr`),
+      ],
+    });
+    const { code, stderr, stdout } = await cmd.output();
+    const td = new TextDecoder();
+    if (code === 0) {
+      console.log(td.decode(stdout).trim());
+    } else {
+      throw new Error(td.decode(stderr));
+    }
+    await Deno.remove(clrJson);
   }),
 ).then(() => Deno.exit(0));
