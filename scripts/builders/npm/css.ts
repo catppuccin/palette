0a3488ffde6440d3e891b7acf5f4f73459d29614
sgoudham <sgoudham@gmail.com>
2025-03-10 22:56:34 +0000

    feat: generate css with tints/shades

--- scripts/builders/npm/css.ts
+++ scripts/builders/npm/css.ts
@@ -1,18 +1,60 @@
 import { sprintf } from "std/fmt/printf.ts";
 import { ensureDir } from "std/fs/mod.ts";
 
-import { flavorEntries } from "@catppuccin/palette";
+import {
+  type AccentName,
+  type CatppuccinShadeColors,
+  type CatppuccinTintColors,
+  flavorEntries,
+} from "@catppuccin/palette";
+
+const blendedColors = (
+  blended: CatppuccinTintColors | CatppuccinShadeColors,
+  prefix: string,
+  accent: AccentName,
+) => {
+  return Object.entries(blended[accent]).flatMap(
+    ([colorName, color]) => [
+      sprintf("  %s-%s: %s;", prefix, colorName, color.hex),
+      sprintf(
+        "  %s-%s-rgb: %d %d %d;",
+        prefix,
+        colorName,
+        ...Object.values(color.rgb),
+      ),
+      sprintf(
+        "  %s-%s-hsl: %.3f %.3f%% %.3f%%;",
+        prefix,
+        colorName,
+        color.hsl.h,
+        color.hsl.s * 100,
+        color.hsl.l * 100,
+      ),
+    ],
+  );
+};
 
 const template = flavorEntries
-  .map(([flavorName, { colorEntries }]) => {
+  .map(([flavorName, { tints, shades, colorEntries }]) => {
     const colors = colorEntries
-      .map(([colorName, { hex, rgb, hsl: { h, s, l } }]) => {
+      .map(([colorName, { accent, hex, rgb, hsl: { h, s, l } }]) => {
         const name = `--ctp-${flavorName}-${colorName}`;
-
-        return [
+        const cssColors = [
           sprintf("  %s: %s;", name, hex),
           sprintf("  %s-rgb: %d %d %d;", name, ...Object.values(rgb)),
           sprintf("  %s-hsl: %.3f %.3f%% %.3f%%;", name, h, s * 100, l * 100),
+        ];
+        const cssTints = accent
+          ? blendedColors(tints, name, colorName as AccentName)
+          : [];
+        const cssShades = accent
+          ? blendedColors(shades, name, colorName as AccentName)
+          : [];
+
+        return [
+          ...cssTints.reverse(),
+          ...cssColors,
+          ...cssShades,
         ].join("\n");
       })
       .join("\n");
