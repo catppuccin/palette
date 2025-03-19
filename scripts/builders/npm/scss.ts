01f5ce1bb199280e5920d71457a29be597f9c577
sgoudham <sgoudham@gmail.com>
2025-03-19 02:08:31 +0000

    feat(scss): add tints/shades to individual files

--- scripts/builders/npm/scss.ts
+++ scripts/builders/npm/scss.ts
@@ -1,6 +1,6 @@
 import { ensureDir } from "std/fs/ensure_dir.ts";
 
-import { flavorEntries } from "@catppuccin/palette";
+import { type CatppuccinFlavor, flavorEntries } from "@catppuccin/palette";
 
 const combined = `$palette: (
 ${
@@ -16,20 +16,44 @@ ${
 }
 );`;
 
+const individualFile = (flavor: CatppuccinFlavor) => {
+  const accentLines = flavor.accentColorEntries.reduce(
+    (acc, [accentName, accentColor]) => {
+      const tintColor = flavor.tints[accentName];
+      const shadeColor = flavor.shades[accentName];
+
+      for (const [name, { hex }] of Object.entries(tintColor).reverse()) {
+        acc += `$${accentName}-${name}: ${hex};\n`;
+      }
+      acc += `$${accentName}: ${accentColor.hex};\n`;
+      for (const [name, { hex }] of Object.entries(shadeColor)) {
+        acc += `$${accentName}-${name}: ${hex};\n`;
+      }
+
+      return acc;
+    },
+    "",
+  );
+  const monochromaticLines = flavor.monochromaticColorEntries.reduce(
+    (acc, [colorName, color]) => {
+      acc += `$${colorName}: ${color.hex};\n`;
+      return acc;
+    },
+    "",
+  );
+  return accentLines + monochromaticLines;
+};
+
 export const compileScss = async (outDir: string) => {
   await ensureDir(`${outDir}/scss`);
 
   // write each flavor to its own file
-  flavorEntries.map(([flavorName, { colorEntries }]) => {
+  flavorEntries.map(([flavorName, flavor]) =>
     Deno.writeTextFile(
       `${outDir}/scss/_${flavorName}.scss`,
-      colorEntries
-        .map(([key, value]) => {
-          return `$${key}: ${value.hex};`;
-        })
-        .join("\n"),
-    );
-  });
+      individualFile(flavor),
+    )
+  );
 
   // and a combined map of all flavors
   Deno.writeTextFile(`${outDir}/scss/_catppuccin.scss`, combined);
